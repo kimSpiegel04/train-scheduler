6abdc0b798bc052901d2404d189e42ad3916f8bb
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-07-05 18:39:58 -0400

    update layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,82 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBblzGHBwydDqaLSgfgZFeZn-qTvlsxMW0",
-    authDomain: "train-project-8994c.firebaseapp.com",
-    databaseURL: "https://train-project-8994c.firebaseio.com",
-    projectId: "train-project-8994c",
-    storageBucket: "train-project-8994c.appspot.com",
-    messagingSenderId: "556033642177",
-    appId: "1:556033642177:web:757c4b803f7173b3"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+$(document).ready(function (){
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyBblzGHBwydDqaLSgfgZFeZn-qTvlsxMW0",
+        authDomain: "train-project-8994c.firebaseapp.com",
+        databaseURL: "https://train-project-8994c.firebaseio.com",
+        projectId: "train-project-8994c",
+        storageBucket: "train-project-8994c.appspot.com",
+        messagingSenderId: "556033642177",
+        appId: "1:556033642177:web:757c4b803f7173b3"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-// Create a variable to reference the database.
-var database = firebase.database();
+    // Create a variable to reference the database.
+    var database = firebase.database();
 
-//display current time (military)
-function currentTime(){
-    var getTime = moment().format('HH:mm');
-    $('.current-time').text(getTime);
-};
+    //display current time (military)
+    function currentTime(){
+        var getHour = moment().format('HH');
+        var getMin = moment().format('mm');
+        $('.hours').text(getHour);
+        $('.min').text(getMin);
+    };
 
-//animations for min away
+    //animations for min away
+    function minChange(k,min){
+        if (min==1){
+            $(`#${k}`).toggleClass('blink');
+            $(`#${k}`).text('ARR');
+        } 
+    };
 
-$('#submit-info').on('click', function(event){
-    event.preventDefault();
+    $('#submit-info').on('click', function(event){
+        event.preventDefault();
 
-    //grab values
-    var trainName=$('#train-name').val().trim();
-    var destination=$('#destination').val().trim();
-    var startTime = $('#start-time').val().trim();
-    var frequency=$('#frequency').val().trim();
+        //grab values
+        var trainName=$('#train-name').val().trim();
+        var destination=$('#destination').val().trim();
+        var startTime = $('#start-time').val().trim();
+        var frequency=$('#frequency').val().trim();
 
-    var newTrain={
-        name: trainName,
-        destination: destination,
-        start: startTime,
-        frequency: frequency,
-        inputDate: firebase.database.ServerValue.TIMESTAMP
-    }
+        var newTrain={
+            name: trainName,
+            destination: destination,
+            start: startTime,
+            frequency: frequency,
+            inputDate: firebase.database.ServerValue.TIMESTAMP
+        }
 
-    //push
-    database.ref().push(newTrain);
+        //push
+        database.ref().push(newTrain);
 
-    //clear input
-    document.getElementById('train-name').value='';
-    document.getElementById('destination').value='';
-    document.getElementById('start-time').value='';
-    document.getElementById('frequency').value='';
-});
+        //clear input
+        document.getElementById('train-name').value='';
+        document.getElementById('destination').value='';
+        document.getElementById('start-time').value='';
+        document.getElementById('frequency').value='';
+    });
 
-setInterval(currentTime,1000);
+    setInterval(currentTime,1000);
 
-database.ref().on('child_added', function(snapshot){
-    //name and destination (no change required)
-    var train = snapshot.val().name;
-    var destination = snapshot.val().destination;
-    var frequency = snapshot.val().frequency;
+    database.ref().on('child_added', function(snapshot){
+        //name and destination (no change required)
+        var key = snapshot.key;
+        var train = snapshot.val().name;
+        var destination = snapshot.val().destination;
+        var frequency = snapshot.val().frequency;
 
-    //calculations 
-    var start = moment(snapshot.val().start, 'HH:mm').subtract(1, 'y');
-    var timeDiff = moment().diff(moment(start), "m");
+        //calculations 
+        var start = moment(snapshot.val().start, 'HH:mm').subtract(1, 'y');
+        var timeDiff = moment().diff(moment(start), "m");
 
-    var remaining = timeDiff % frequency;
-    var minAway = frequency - remaining;
-    var nextArrival = moment().add(minAway, "minutes").format("HH:mm");;
+        var remaining = timeDiff % frequency;
+        var minAway = frequency - remaining;
+        var nextArrival = moment().add(minAway, "minutes").format("HH:mm");;
 
-    $(".info-table").append("<tr><td>" + train + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td class='min-away'>" + minAway + "</td></tr>");    
+        $(".info-table").append(`<tr><td>${train}</td><td>${destination}</td><td>${frequency}</td><td>${nextArrival}</td><td class='min-away' id=${key}>${minAway}</td></tr>`);
+        
+        minChange(key,minAway);
+    });
 });
